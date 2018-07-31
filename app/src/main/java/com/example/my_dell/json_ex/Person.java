4f58c13e352a7d09f332ce5836856ab52cdb185d
my-dell <dsrivastava565@gmail.com>
2018-07-31 22:20:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/my_dell/json_ex/Person.java
@@ -0,0 +1,31 @@
+package com.example.my_dell.json_ex;
+
+public class Person{
+    private String name;
+    private String country;
+    private String twitter;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public String getTwitter() {
+        return this.twitter;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+}
